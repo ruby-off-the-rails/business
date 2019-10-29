843098fa795e247129c0bceb9cf3e4219180d14d
brian <riceb53@gmail.com>
2019-10-28 19:31:11 -0500

    add employee as array

---
+++ employee.rb
@@ -0,0 +1,16 @@
+# employee has first name, last name, active status, salary
+
+# "Majora Carter makes $80000 per year."
+
+# array
+employee1 = ["Marjora", "Carter", true, 80000]
+employee2 = ["Danilo", "Campos", false, 70000]
+
+p employee1[0] + " " + employee1[1] + " makes $" + employee1[3].to_s + " per year."
+
+p "#{employee2[0]} #{employee2[1]} makes $#{employee2[3]} per year."
+
+# hash
+
+
+# class
