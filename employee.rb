8fc2e5a91529469e9700de7c1ed770f818918a6f
brian <riceb53@gmail.com>
2019-10-28 20:39:53 -0500

    add give_annual_raise, getter/setter methods

--- employee.rb
+++ employee.rb
@@ -10,18 +10,63 @@
 
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[3]} per year."
 
-# hash
-employee1 = {"first_name" => "Majora", "last_name" => "Carter", "active" => true, "salary" => 80000}
+# # hash
+# employee1 = {"first_name" => "Majora", "last_name" => "Carter", "active" => true, "salary" => 80000}
 
-employee2 = {:first_name => "Danilo", :last_name => "Campos", :active => false, :salary => 70000}
+# employee2 = {:first_name => "Danilo", :last_name => "Campos", :active => false, :salary => 70000}
 
-# fancy js syntax
-# employee2 = {first_name: "Danilo", last_name: "Campos", active: false, salary: 70000}
+# # fancy js syntax
+# # employee2 = {first_name: "Danilo", last_name: "Campos", active: false, salary: 70000}
 
 
-p "#{employee1["first_name"]} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+# p "#{employee1["first_name"]} #{employee1['last_name']} makes $#{employee1['salary']} per year."
 
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 # p "#{employee1}"
 
 # class
+
+class Employee
+  # getter/reader
+  def first_name
+    @first_name
+  end
+
+  # setter/writer
+  def first_name=(input_first_name)
+    @first_name = input_first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def initialize(input_first_name, input_last_name, input_active, input_salary)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @active = input_active
+    @salary = input_salary
+  end
+
+  def print_info
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+  end
+
+  def give_annual_raise
+    @salary *= 1.05
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", true, 80000)
+employee1.print_info
+
+employee2 = Employee.new("Danilo", "Campos", true, 70000)
+# employee2.print_info
+
+# employee1.give_annual_raise
+# employee1.print_info
+
+
+employee1.first_name = "Brian"
+
+employee1.print_info
